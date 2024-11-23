e943a7884d7d8598298115dbede2539d4f3bac2d
Md Wasiul Islam <99739954+mislam77@users.noreply.github.com>
2024-11-23 02:38:47 -0500

    small fix

--- server/server.ts
+++ server/server.ts
@@ -52,7 +52,7 @@ app.use("/api/transactions", transactionsRoutes);
 app.use("/api/categories", categoryRoutes);
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
